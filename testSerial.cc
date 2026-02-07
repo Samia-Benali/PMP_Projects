c80eb565b6437aadd75ef8a19d3404b9f00fd6ff
KilianGaillard <kiliangaillard25@gmail.com>
2026-02-07 11:30:41 +0100

    début de OBinaryFile et constrcuteur qui marche

--- testSerial.cc
+++ testSerial.cc
@@ -4,6 +4,10 @@
 
 #include "config.h"
 
+TEST(TestSerial, OBinaryFileConstructorNoError) {
+  EXPECT_NO_THROW({serial::OBinaryFile file("non_existent_file.bin");});
+}
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();

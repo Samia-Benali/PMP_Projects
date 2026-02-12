30febe2a6522f098fa2ae558b3857e51a688b518
Samia <sbenali.68000@gmail.com>
2026-02-12 13:27:06 +0100

    modif pour fonctionnement des template avec b et l enfian

--- Serial.cc
+++ Serial.cc
@@ -1,53 +1,74 @@
 #include "Serial.h"
 #include <cstdio>
-#include <stdexcept>
 #include <utility>
+#include <cstdint>
+#include <cstring> 
+#include <cstddef> 
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
 
 namespace {
 
-  // https://keasigmadelta.com/blog/how-to-convert-endianness-in-c-c-in-4-different-ways/
-    uint16_t swap16(uint16_t val) {
-        return ((0xFF00 & val ) >> 8) | ((0xFF00 & val ) << 8);
-    }
-
-    uint32_t swap32(uint32_t val) {
-        return ((0xFF00000000 & val) >> 24) |
-         ((0x000000FF & val) << 24) |
-         ((0x00FF0000 & val ) >> 8) |
-         ((0x00FF0000 & val ) << 8);
-    }
-
-    uint64_t swap64(uint64_t val) {
-        return ((0xFF00000000000000 & val) >> 56) |
-         ((0x00000000000000FF & val) << 56) | 
-         ((0x00FF000000000000 & val) >> 40) | 
-         ((0x000000000000FF00 & val) >> 40) | 
-         ((0x0000FF0000000000 & val) >> 24) | 
-         ((0x0000000000FF0000 & val) << 24) | 
-          ((0x000000FF00000000 & val) >> 8) | 
-          ((0x00000000FF000000 & val) << 8) ;
-    }
-
 
     template <typename T>
-    T swap_endian(T val) {
-        if (sizeof(T) == 1){
-            return val;
+    void to_bytes(T val, std::byte* buf) {
+        // https://en.cppreference.com/w/cpp/types/make_unsigned.html
+        using U = std::make_unsigned_t<T>;
+        U uval = static_cast<U>(val);
+
+        if constexpr (sizeof(T) == 2) {
+            buf[0] = static_cast<std::byte>((uval >> 8) & 0xFF);
+            buf[1] = static_cast<std::byte>(uval & 0xFF);
         }
-        if(T is uint16_t)
-        {
-          swap16(val);
+        else if constexpr (sizeof(T) == 4) {
+            buf[0] = static_cast<std::byte>((uval >> 24) & 0xFF);
+            buf[1] = static_cast<std::byte>((uval >> 16) & 0xFF);
+            buf[2] = static_cast<std::byte>((uval >> 8) & 0xFF);
+            buf[3] = static_cast<std::byte>(uval & 0xFF);
         }
-        else if(T is uint32_t)
-        {
-          swap32(val);
-        }
-        else if (T is u_int64_t)
-        {
-          swap64(val);
+        else if constexpr (sizeof(T) == 8) {
+            buf[0] = static_cast<std::byte>((uval >> 56) & 0xFF);
+            buf[1] = static_cast<std::byte>((uval >> 48) & 0xFF);
+            buf[2] = static_cast<std::byte>((uval >> 40) & 0xFF);
+            buf[3] = static_cast<std::byte>((uval >> 32) & 0xFF);
+            buf[4] = static_cast<std::byte>((uval >> 24) & 0xFF);
+            buf[5] = static_cast<std::byte>((uval >> 16) & 0xFF);
+            buf[6] = static_cast<std::byte>((uval >> 8) & 0xFF);
+            buf[7] = static_cast<std::byte>(uval & 0xFF);
         }
     }
 
+    template <typename T>
+    T from_bytes(const std::byte* buf) {
+        using U = std::make_unsigned_t<T>;
+        U uval = 0;
+
+        if constexpr (sizeof(T) == 2) {
+            uval = (static_cast<U>(buf[0]) << 8) |
+                   (static_cast<U>(buf[1]));
+        }
+        else if constexpr (sizeof(T) == 4) {
+            uval = (static_cast<U>(buf[0]) << 24) |
+                   (static_cast<U>(buf[1]) << 16) |
+                   (static_cast<U>(buf[2]) << 8)  |
+                   (static_cast<U>(buf[3]));
+        }
+        else if constexpr (sizeof(T) == 8) {
+            uval = (static_cast<U>(buf[0]) << 56) |
+                   (static_cast<U>(buf[1]) << 48) |
+                   (static_cast<U>(buf[2]) << 40) |
+                   (static_cast<U>(buf[3]) << 32) |
+                   (static_cast<U>(buf[4]) << 24) |
+                   (static_cast<U>(buf[5]) << 16) |
+                   (static_cast<U>(buf[6]) << 8)  |
+                   (static_cast<U>(buf[7]));
+        }
+        
+        return static_cast<T>(uval);
+    }
 }
 
 
@@ -94,110 +115,97 @@ namespace serial {
         return *this = IBinaryFile(other);
     }
 
-    //Move Assignement
+    //Move Assignement 
     IBinaryFile& IBinaryFile::operator=(IBinaryFile&& other) noexcept {
         std::swap(file_bi, other.file_bi);
         std::swap(filename, other.filename);
         return *this;
     }
 
+    std::size_t IBinaryFile::read(std::byte* buffer, std::size_t size) {
+        std::size_t reading = std::fread(buffer, 1, size, file_bi);
+        if (reading < size && std::ferror(file_bi)) {
+             perror("Read couldn't work");
+             return reading;
+        }
+        return reading;
+    }
+
     
     //////////////////////////////////////////////////////////////
     //                      Operator IBinary                    //
     //////////////////////////////////////////////////////////////
+
+    template<typename T>
+    void read_type(IBinaryFile& file, T& x) {
+        std::byte buffer[sizeof(T)];
+        file.read(buffer, sizeof(T));
+        x = from_bytes<T>(buffer);
+    }
+
     IBinaryFile& operator>>(IBinaryFile& file, uint8_t& x) {
-        file.read(reinterpret_cast<std::byte*>(&x), sizeof(x));
+        file.read(reinterpret_cast<std::byte*>(&x), 1);
         return file;
     }
 
     IBinaryFile& operator>>(IBinaryFile& file, int8_t& x) {
-        file.read(reinterpret_cast<std::byte*>(&x), sizeof(x));
+        file.read(reinterpret_cast<std::byte*>(&x), 1);
         return file;
     }
 
-    IBinaryFile& operator>>(IBinaryFile& file, uint16_t& x) {
-        uint16_t temp;
-        file.read(reinterpret_cast<std::byte*>(&temp), sizeof(temp));
-        x = swap_endian(temp);
-        return file;
+    IBinaryFile& operator>>(IBinaryFile& file, uint16_t& x) { 
+        read_type(file, x); return file; 
     }
-
-    IBinaryFile& operator>>(IBinaryFile& file, int16_t& x) {
-        uint16_t temp;
-        file.read(reinterpret_cast<std::byte*>(&temp), sizeof(temp));
-        x = static_cast<int16_t>(swap_endian(temp));
-        return file;
+    IBinaryFile& operator>>(IBinaryFile& file, int16_t& x)  { 
+        read_type(file, x); return file; 
     }
-
-    IBinaryFile& operator>>(IBinaryFile& file, uint32_t& x) {
-        uint32_t temp;
-        file.read(reinterpret_cast<std::byte*>(&temp), sizeof(temp));
-        x = swap_endian(temp);
-        return file;
+    IBinaryFile& operator>>(IBinaryFile& file, uint32_t& x) { 
+        read_type(file, x); return file; 
     }
-
-    IBinaryFile& operator>>(IBinaryFile& file, int32_t& x) {
-        uint32_t temp;
-        file.read(reinterpret_cast<std::byte*>(&temp), sizeof(temp));
-        x = static_cast<int32_t>(swap_endian(temp));
-        return file;
+    IBinaryFile& operator>>(IBinaryFile& file, int32_t& x)  { 
+        read_type(file, x); return file; 
     }
-
-    IBinaryFile& operator>>(IBinaryFile& file, uint64_t& x) {
-        uint64_t temp;
-        file.read(reinterpret_cast<std::byte*>(&temp), sizeof(temp));
-        x = swap_endian(temp);
-
-        return file;
+    IBinaryFile& operator>>(IBinaryFile& file, uint64_t& x) { 
+        read_type(file, x); return file; 
     }
-
-    IBinaryFile& operator>>(IBinaryFile& file, int64_t& x) {
-        uint64_t temp;
-        file.read(reinterpret_cast<std::byte*>(&temp), sizeof(temp));
-        x = static_cast<int64_t>(swap_endian(temp));
-        return file;
+    IBinaryFile& operator>>(IBinaryFile& file, int64_t& x)  { 
+        read_type(file, x); return file; 
     }
 
     IBinaryFile& operator>>(IBinaryFile& file, char& x) {
-        file.read(reinterpret_cast<std::byte*>(&x), sizeof(x));
+        file.read(reinterpret_cast<std::byte*>(&x), 1);
         return file;
     }
 
     IBinaryFile& operator>>(IBinaryFile& file, float& x) {
-        file.read(reinterpret_cast<std::byte*>(&x), sizeof(x));
+        uint32_t temp;
+        read_type(file, temp);
+        std::memcpy(&x, &temp, sizeof(float));
         return file;
     }
 
     IBinaryFile& operator>>(IBinaryFile& file, double& x) {
-        file.read(reinterpret_cast<std::byte*>(&x), sizeof(x));
+        uint64_t temp;
+        read_type(file, temp);
+        std::memcpy(&x, &temp, sizeof(double));
         return file;
     }
 
     IBinaryFile& operator>>(IBinaryFile& file, bool& x) {
-        file.read(reinterpret_cast<std::byte*>(&x), sizeof(x));
+        uint8_t temp;
+        file >> temp;
+        x = (temp != 0);
         return file;
     }
 
     IBinaryFile& operator>>(IBinaryFile& file, std::string& x) {
         uint64_t size;
-        file >> size;
+        file >> size; 
         x.resize(size);
         if (size > 0) {
-            file.read(reinterpret_cast<std::byte*>(&x[0]), size);
+            file.read(reinterpret_cast<std::byte*>(x.data()), size);
         }
         return file;
-    }
-
-    //Read
-    std::size_t IBinaryFile::read(std::byte* data, std::size_t size) {
-        if (file_bi == nullptr){
-            return 0;
-        } 
-        std::size_t res = std::fread(data, 1, size, file_bi);
-        if (res < size){
-            perror("fread");
-            return res;
-        }
     }
 
 
@@ -240,7 +248,7 @@ namespace serial {
       mode(other.mode) {
     }
 
-    //Move Constructor
+    //Move Constructor 
     OBinaryFile& OBinaryFile::operator=(const OBinaryFile& other) {
         return *this = OBinaryFile(other);
     }
@@ -255,13 +263,13 @@ namespace serial {
 
     //https://www.developpez.net/forums/d1113169/c-cpp/cpp/conversion-little-endian-big-endian-inversement/
 
-    //Write
     std::size_t OBinaryFile::write(const std::byte* data, std::size_t size) {
         std::size_t writting = fwrite(data,1,size,file_bo);
         if(writting < size) {
             perror("Write couldn't work");
             return writting;
         }
+        return writting;
     }
 
 
@@ -269,94 +277,78 @@ namespace serial {
     //////////////////////////////////////////////////////////////
     //                      Operator OBinary                    //
     /////////////////////////////////////////////////////////////
+    
+    template<typename T>
+    void write_type(OBinaryFile& file, T x) {
+        std::byte buffer[sizeof(T)];
+        to_bytes(x, buffer); 
+        file.write(buffer, sizeof(T));  
+    }
+
     OBinaryFile& operator<<(OBinaryFile& file, uint8_t x) {
-        file.write(reinterpret_cast<const std::byte*>(&x), sizeof(x));
+        file.write(reinterpret_cast<const std::byte*>(&x), 1);
         return file;
     }
-
+    
     OBinaryFile& operator<<(OBinaryFile& file, int8_t x) {
-        file.write(reinterpret_cast<const std::byte*>(&x), sizeof(x));
+        file.write(reinterpret_cast<const std::byte*>(&x), 1);
         return file;
     }
 
-    OBinaryFile& operator<<(OBinaryFile& file, uint16_t x) {
-        uint16_t temp = x;
-        uint16_t swapped = swap_endian(temp);
-        file.write(reinterpret_cast<const std::byte*>(&swapped), sizeof(x));
-        return file;
+    OBinaryFile& operator<<(OBinaryFile& file, uint16_t x) { 
+        write_type(file, x); return file; 
     }
 
-    OBinaryFile& operator<<(OBinaryFile& file, int16_t x) {
-        uint16_t temp = static_cast<uint16_t>(x);
-        uint16_t swapped = swap_endian(temp);
-        file.write(reinterpret_cast<const std::byte*>(&swapped), sizeof(x));
-        return file;
+    OBinaryFile& operator<<(OBinaryFile& file, int16_t x)  { 
+        write_type(file, x); return file; 
     }
 
-    OBinaryFile& operator<<(OBinaryFile& file, uint32_t x) {
-        uint16_t temp = x;
-        uint16_t swapped = swap_endian(temp);
-        file.write(reinterpret_cast<const std::byte*>(&swapped), sizeof(x));
-        return file;
+    OBinaryFile& operator<<(OBinaryFile& file, uint32_t x) { 
+        write_type(file, x); return file; 
     }
 
-    OBinaryFile& operator<<(OBinaryFile& file, int32_t x) {
-        uint32_t temp = static_cast<uint32_t>(x);
-        uint32_t swapped = swap_endian(temp);
-        file.write(reinterpret_cast<const std::byte*>(&swapped), sizeof(x));
-        return file;
+    OBinaryFile& operator<<(OBinaryFile& file, int32_t x)  { 
+        write_type(file, x); return file; 
+    }
+    OBinaryFile& operator<<(OBinaryFile& file, uint64_t x) { 
+        write_type(file, x); return file; 
     }
 
-    OBinaryFile& operator<<(OBinaryFile& file, uint64_t x) {
-        uint64_t temp = x;
-        uint64_t swapped = swap_endian(temp);
-        file.write(reinterpret_cast<const std::byte*>(&swapped), sizeof(x));
-        return file;
-    }
-
-    OBinaryFile& operator<<(OBinaryFile& file, int64_t x) {
-        uint64_t temp = static_cast<uint64_t>(x);
-        uint64_t swapped = swap_endian(temp);
-        file.write(reinterpret_cast<const std::byte*>(&swapped), sizeof(x));
-        return file;
+    OBinaryFile& operator<<(OBinaryFile& file, int64_t x)  { 
+        write_type(file, x); return file; 
     }
 
     OBinaryFile& operator<<(OBinaryFile& file, char x) {
-        uint8_t temp = x;
-        uint8_t swapped = swap_endian(temp);
-        file.write(reinterpret_cast<const std::byte*>(&swapped), sizeof(x));
+        file.write(reinterpret_cast<const std::byte*>(&x), 1);
         return file;
     }
 
     OBinaryFile& operator<<(OBinaryFile& file, float x) {
-        file.write(reinterpret_cast<const std::byte*>(&x), sizeof(x));
+        uint32_t temp;
+        std::memcpy(&temp, &x, sizeof(float));
+        write_type(file, temp);
         return file;
     }
 
     OBinaryFile& operator<<(OBinaryFile& file, double x) {
-        file.write(reinterpret_cast<const std::byte*>(&x), sizeof(x));
+        uint64_t temp;
+        std::memcpy(&temp, &x, sizeof(double));
+        write_type(file, temp);
         return file;
     }
 
     OBinaryFile& operator<<(OBinaryFile& file, bool x) {
-        file.write(reinterpret_cast<const std::byte*>(&x), sizeof(x));
+        uint8_t temp = x ? 1 : 0;
+        file << temp;
         return file;
     }
 
     OBinaryFile& operator<<(OBinaryFile& file, const std::string& x) {
-        uint64_t temp = x;
-        uint64_t swapped = swap_endian(temp);
-        uint64_t size = swapped.size();
+        uint64_t size = x.size();
         file << size;
         if (size > 0) {
-            file.write(reinterpret_cast<const std::byte*>(swapped.data()), size);
+            file.write(reinterpret_cast<const std::byte*>(x.data()), size);
         }
         return file;
     }
-
-
 }
-
-
-
-

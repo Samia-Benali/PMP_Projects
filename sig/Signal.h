9e95e2660e5a3c6ec25fdd14da67e454de014642
Samia <sbenali.68000@gmail.com>
2026-03-11 10:53:55 +0100

    ajout des signaux

--- sig/Signal.h
+++ sig/Signal.h
@@ -2,6 +2,7 @@
 #define SIGNAL_H
 
 #include <optional>
+#include <vector>
 
 namespace sig {
 
@@ -11,7 +12,7 @@ namespace sig {
 
     template<typename U>
     void combine(U item) {
-      
+      (void)item;
     }
 
     result_type result() {
@@ -20,24 +21,28 @@ namespace sig {
 
   template<typename T>
   class LastCombiner {
+  private : 
+    T last;
+
   public:
     using result_type = T;
 
     template<typename U>
     void combine(U item) {
-      last = test;// Gérer le cas pour réussir à mettre item dans last std::move(item);
+      last =  std::move(item);
     }
 
     result_type result() {
       return last;
     }
   
-  private : 
-    T last;
   };
 
   template<typename T>
   class VectorCombiner {
+  private:
+      std::vector<T> vect;
+
   public:
     using result_type = std::vector<T>;
     
@@ -51,8 +56,6 @@ namespace sig {
       return vect;
     }
 
-    private:
-      std::vector<T> vect;
   };
 
   enum class PredicateType {
@@ -63,7 +66,7 @@ namespace sig {
   template<typename T, PredicateType PType = PredicateType::Binary>
   class PredicateCombiner {
   public:
-    using result_type = std::optional</* implementation defined */>;
+    using result_type = std::optional<T>;
 
     PredicateCombiner(/* implementation defined */ predicate) {
       // implementation defined
@@ -79,33 +82,54 @@ namespace sig {
     }
   };
 
+
+
+
   template<typename Signature, typename Combiner = DiscardCombiner>
-  class Signal {
+  class Signal;
+  template<typename R, typename... Args, typename Combiner>
+  class Signal<R(Args...), Combiner> {
+
+   private : 
+    std::size_t next_id = 0;
+    Combiner combiner_val;
+    std::map<std::size_t, std::function<R(Args...)>> slots;
+  
   public:
     using combiner_type = Combiner;
 
-    using result_type = Combiner::result_type;
+    using result_type = typename Combiner::result_type;
 
-    Signal(Combiner combiner = Combiner()) {
-      // implementation defined
-    }
+    Signal(Combiner combiner = Combiner()) 
+      : combiner_val(std::move(combiner)){}
 
+    
     template<typename... CombinerArgs>
-    Signal(CombinerArgs ... args) {
-      // implementation defined
-    }
+    Signal(CombinerArgs ... args) 
+      : combiner_val(std::move<CombinerArgs>(args)...) {}
 
-    std::size_t connectSlot(std::function<Signature> callback) {
-      // implementation defined
+
+    std::size_t connectSlot(std::function<R(Args...)> callback) {
+      std::size_t id = next_id++;
+      slots[id] = std::move(callback);
+      return id;
     }
 
     void disconnectSlot(std::size_t id) {
-      // implementation defined
+      slots.erase(id);
     }
 
-    result_type emitSignal(/* implementation defined */) {
-      // implementation defined
+    result_type emitSignal(CombinerArgs... args) {
+      for (auto& [id, callback] : slots) {
+        if constexpr (std::is_void_v<R>) {
+            callback(args...);
+        } else {
+            combiner_val.combine(callback(args...));
+        }
+      }
+      return combiner_val.result();
     }
+
   };
 
 }
